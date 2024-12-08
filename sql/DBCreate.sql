4ca5bee2fc511baef76da806c031ec3bf24a2ad3
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com.>
2024-12-08 12:34:33 -0600

    added FK in tables

--- sql/DBCreate.sql
+++ sql/DBCreate.sql
@@ -3,17 +3,17 @@ USE GDA00545OT_AngelMarroquin;
 
 CREATE TABLE Roles (
 	RolID int PRIMARY KEY IDENTITY(1,1), 
-	NameRol varchar(30) NOT NULL,
+	NombreRol varchar(30) NOT NULL,
 );
 
 CREATE TABLE Estados (
 	EstadoId int PRIMARY KEY IDENTITY(1,1),
-	NameEstado varchar(30) NOT NULL,
+	NombreEstado varchar(30) NOT NULL,
 );
 
 CREATE TABLE Marcas (
 	MarcaId int PRIMARY KEY IDENTITY(1,1),
-	NameMarca varchar(30) NOT NULL,
+	NombreMarca varchar(30) NOT NULL,
 );
 
 CREATE TABLE Clientes (
@@ -31,32 +31,40 @@ CREATE TABLE Usuarios(
 	RolId int NOT NULL,
 	EstadoId int,
 	Email varchar(50) NOT NULL,
-	NameUsuario varchar(100) NOT NULL,
+	NombreUsuario varchar(100) NOT NULL,
 	PasswordUsuario varchar(100) NOT NULL,
 	Telefono int,
 	BirthDate date,
-	CreatedDate datetime NOT NULL
+	CreatedDate datetime NOT NULL,
+	CONSTRAINT FKUsuarios_RolId FOREIGN KEY (RolID) REFERENCES Roles(RolId),
+	CONSTRAINT FKUsuarios_EstadoId FOREIGN KEY (EstadoID) REFERENCES Estados(EstadoId)
 );
 
 CREATE TABLE CategoriaProductos(
 	CategoriaProductoId int PRIMARY KEY IDENTITY(1,1),
-	NameCategoria varchar(30) NOT NULL,
+	NombreCategoria varchar(30) NOT NULL,
 	EstadoId int NOT NULL,
 	CreatedDate datetime NOT NULL,
-	CreatedBy int NOT NULL
+	CreatedBy int NOT NULL,
+	CONSTRAINT FKCateProductos_EstadoId FOREIGN KEY (EstadoId) REFERENCES Estados(EstadoId),
+	CONSTRAINT FKCateProductos_CreatedBy FOREIGN KEY (CreatedBy) REFERENCES Usuarios(UsuarioId)
 );
 
 CREATE TABLE Productos(
 	ProductoId int PRIMARY KEY IDENTITY(1,1),
 	CategoriaProductoId int NOT NULL, 
-	NameProduct varchar(100) NOT NULL,
+	NombreProducto varchar(100) NOT NULL,
 	MarcaId int NOT NULL,
 	Codigo varchar(50),
 	Stock float NOT NULL,
 	EstadoID int NOT NULL,
 	PrecioUnidad decimal NOT NULL,
 	CreatedDate datetime NOT NULL,
-	CreatedBy int NOT NULL
+	CreatedBy int NOT NULL,
+	CONSTRAINT FKProductos_CategoriaProductoId FOREIGN KEY (CategoriaProductoId) REFERENCES CategoriaProductos(CategoriaProductoId),
+	CONSTRAINT FKProductos_MarcaId FOREIGN KEY (MarcaId) REFERENCES Marcas(MarcaId),
+	CONSTRAINT FKProductos_EstadoId FOREIGN KEY (EstadoId) REFERENCES Estados(EstadoId),
+	CONSTRAINT FKProductos_CreatedBy FOREIGN KEY (CreatedBy) REFERENCES Usuarios(UsuarioId)
 );
 
 
@@ -71,14 +79,20 @@ CREATE TABLE OrderHeader(
 	FechaEntrega date NOT NULL,
 	DetallesEntrega varchar(200) NOT NULL,
 	TotalOrder float NOT NULL,
-	CreateDate datetime NOT NULL
+	CreateDate datetime NOT NULL,
+	CONSTRAINT FKOrderHeader_UsuarioId FOREIGN KEY (UsuarioId) REFERENCES Usuarios(UsuarioId),
+	CONSTRAINT FKOrderHeader_EstadoId FOREIGN KEY (EstadoId) REFERENCES Estados(EstadoId)
 );
 
 CREATE TABLE OrderDetails(
 	OrderDetailId int PRIMARY KEY IDENTITY(1,1),
 	OrderId int NOT NULL,
-	ProductId int NOT NULL,
+	ProductoId int NOT NULL,
 	Cantidad int NOT NULL,
 	Precio decimal NOT NULL,
-	Subtotal decimal NOT NULL
+	Subtotal decimal NOT NULL,
+	CONSTRAINT FKOrderDetail_OrderId FOREIGN KEY (OrderId) REFERENCES OrderHeader(OrderId),
+	CONSTRAINT FKOrderDetail_ProductId FOREIGN KEY (ProductoId) REFERENCES Productos(ProductoId)
 );
+
+
